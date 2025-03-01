b7deab1d249ab6db7962d9094faf028f3bc1bed3
y0112304 <judemawad@gmail.com>
2025-03-01 18:15:25 +0100

    Initial Commit

---
+++ Calorie Hunter/Calorie_HunterApp.swift
@@ -0,0 +1,20 @@
+//
+//  Calorie_HunterApp.swift
+//  Calorie Hunter
+//
+//  Created by Jude Mawad on 01.03.25.
+//
+
+import SwiftUI
+
+@main
+struct Calorie_HunterApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ Calorie HunterTests/Calorie_HunterTests.swift
@@ -0,0 +1,17 @@
+//
+//  Calorie_HunterTests.swift
+//  Calorie HunterTests
+//
+//  Created by Jude Mawad on 01.03.25.
+//
+
+import Testing
+@testable import Calorie_Hunter
+
+struct Calorie_HunterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
