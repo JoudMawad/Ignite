590e01260b763fc3927066edf05c143624d21e98
y0112304 <judemawad@gmail.com>
2025-04-28 11:34:42 +0200

    FIX Can't add the Same Food Twice

--- Calorie Hunter/CoreData/FoodEntity+CoreDataProperties.swift
+++ Calorie Hunter/CoreData/FoodEntity+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  FoodEntity+CoreDataProperties.swift
 //  Calorie Hunter
 //
-//  Created by Jude Mawad on 27.04.25.
+//  Created by Jude Mawad on 28.04.25.
 //
 //
 
@@ -16,18 +16,35 @@ extension FoodEntity {
         return NSFetchRequest<FoodEntity>(entityName: "FoodEntity")
     }
 
-    @NSManaged public var name: String?
+    @NSManaged public var barcode: String?
     @NSManaged public var calories: Double
-    @NSManaged public var protein: Double
     @NSManaged public var carbs: Double
+    @NSManaged public var date: Date?
     @NSManaged public var fat: Double
     @NSManaged public var grams: Double
-    @NSManaged public var mealType: String?
-    @NSManaged public var date: Date?
-    @NSManaged public var isUserAdded: Bool
-    @NSManaged public var barcode: String?
     @NSManaged public var id: UUID?
-    @NSManaged public var consumptions: ConsumptionEntity?
+    @NSManaged public var isUserAdded: Bool
+    @NSManaged public var mealType: String?
+    @NSManaged public var name: String?
+    @NSManaged public var protein: Double
+    @NSManaged public var consumptions: NSSet?
+
+}
+
+// MARK: Generated accessors for consumptions
+extension FoodEntity {
+
+    @objc(addConsumptionsObject:)
+    @NSManaged public func addToConsumptions(_ value: ConsumptionEntity)
+
+    @objc(removeConsumptionsObject:)
+    @NSManaged public func removeFromConsumptions(_ value: ConsumptionEntity)
+
+    @objc(addConsumptions:)
+    @NSManaged public func addToConsumptions(_ values: NSSet)
+
+    @objc(removeConsumptions:)
+    @NSManaged public func removeFromConsumptions(_ values: NSSet)
 
 }
 
